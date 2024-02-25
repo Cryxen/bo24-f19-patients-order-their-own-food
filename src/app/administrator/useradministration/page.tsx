3540c7c50bdd9926a64bfa0c571df706ca293484
HippedFever <Matsc99@gmail.com>
2024-02-25 14:55:46 +0100

    Admin static pages
    Administrative static pages for dashboard, budget and user admin

---
+++ src/app/administrator/useradministration/page.tsx
@@ -0,0 +1,53 @@
+import Layout from '@/app/components/layout'
+import Rolledrop from '@/app/components/Rolledrop'
+import '../../styles/useradministration.scss'
+
+
+
+const Budget = () => {
+    return (
+        <Layout>
+        <div className="mainDiv">
+            <h1>Brukeradministrasjon</h1>
+            <div className='mainbox'>
+                <article className='itembox'>
+                    <section className='contentitembox'><p>BRUKER</p></section>
+                    <section className='contentitemboxrolle'><Rolledrop/></section>
+                </article>
+
+                <article className='itembox'>
+                    <section className='contentitembox'><p>BRUKER</p></section>
+                    <section className='contentitemboxrolle'><Rolledrop/></section>
+                </article>
+
+                <article className='itembox'>
+                    <section className='contentitembox'><p>BRUKER</p></section>
+                    <section className='contentitemboxrolle'><Rolledrop/></section>
+                </article>
+
+                <article className='centerbox'>
+                    <button className='generatebutton'>Oppdater</button>
+                </article>
+
+            </div>
+
+            <div className='mainbox'>
+            <article className='itembox2'>
+                    <section className='contentitemboxrolle'><input type='text' placeholder='Brukernavn' className='inputbruker'></input></section>
+                    <section className='contentitemboxrolle'><input type='text' placeholder='Passord' className='inputbruker'></input></section>
+                </article>
+
+                <article className='centerbox'>
+                    <section className='contentitemboxrolle'><Rolledrop/></section>
+                </article>
+
+                <article className='centerbox'>
+                    <button className='generatebutton'>Opprett profil</button>
+                </article>
+            </div>
+
+        </div>
+        </Layout>
+    )
+}
+export default Budget
