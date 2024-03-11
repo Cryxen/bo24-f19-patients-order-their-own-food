eb2b88960db39c70aa290329b28ecd7846a35f48
Håkon <hakons>
2024-03-11 11:00:24 +0100

    moved login to component and placed it in index

---
+++ src/app/components/Login.tsx
@@ -0,0 +1,66 @@
+"use client"
+import '../styles/loginpage.scss'
+import '../styles/globals.scss'
+import Layout from '../components/layout'
+import { ChangeEvent, MouseEvent, useState } from 'react'
+import { User } from '@/features/users/types'
+import { useRouter } from 'next/navigation'
+
+const Login = () => {
+    //TODO: ERROR HANDLING.
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    //https://nextjs.org/docs/pages/building-your-application/routing/redirecting
+    const router = useRouter()
+
+
+    //https://stackoverflow.com/a/42645711
+    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmitButton = async (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        let user: User
+        const response = await fetch(`/api/users?email=${email}&password=${password}`)
+        if (response.status == 200) {
+            const data = await response.json()
+            if (data.success === true) {
+                user = data.data
+                //Route according to role
+                switch (user.role) {
+                    case "healthcare":
+                        router.push('/healthcareworker')
+                        break;
+                    case "kitchen":
+                        router.push('/kitchenstaff')
+                        break;
+                    case "administrator":
+                        router.push('/administrator')
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    return (
+            <div className='mainDiv'>
+                <h1>Logo</h1>
+                <form>
+                    <input type="email" value={email} onChange={handleEmailChange} placeholder='E-post' />
+                    <input type="password" value={password} onChange={handlePasswordChange} placeholder='Passord' />
+                    <button type="submit" onClick={handleSubmitButton}>Login</button>
+                </form>
+            </div>
+    )
+}
+
+export default Login
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Layout from './components/layout'
+import Login from './components/Login'
 
 export default function Home() {
   return (
     <Layout>
     <h1>bo24-f19-patients-order-their-own-food</h1>
+    <Login />
      </Layout>
   )
 }
