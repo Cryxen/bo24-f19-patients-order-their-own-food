14be6db77827793105799c399ac38c987329c826
Håkon <hakons>
2024-03-11 10:39:08 +0100

    Working API call

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,41 @@
+"use client"
 import '../styles/loginpage.scss'
 import '../styles/globals.scss'
 import Layout from '../components/layout'
-export default function Login () {
+import { ChangeEvent, MouseEvent, useState } from 'react'
+import { User } from '@/features/users/types'
+export default function Login() {
+    const [email, setEmail] = useState('E-post')
+    const [password, setPassword] = useState('Passord')
+
+    //https://stackoverflow.com/a/42645711
+    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmitButton = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        let user: User
+        fetch(`/api/users?email=${email}&password=${password}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }
+
     return (
         <Layout>
-        <div className='mainDiv'>
-            <h1>Logo</h1>
-            <form>
-                <input type="text" defaultValue={"Username"} />
-                <input type="text" defaultValue={"Password"} />
-                <button type="submit">Login</button>
-            </form>
-        </div>
+            <div className='mainDiv'>
+                <h1>Logo</h1>
+                <form>
+                    <input type="text" value={email} onChange={handleEmailChange} />
+                    <input type="text" value={password} onChange={handlePasswordChange} />
+                    <button type="submit" onClick={handleSubmitButton}>Login</button>
+                </form>
+            </div>
         </Layout>
     )
 }
