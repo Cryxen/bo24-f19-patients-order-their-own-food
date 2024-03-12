d4ff1e62bb1726ca6bad5080c056c2dc567c1543
Håkon <hakons>
2024-03-12 11:36:47 +0100

    Made users show from db and rolldown update user object

--- src/app/administrator/useradministration/page.tsx
+++ src/app/administrator/useradministration/page.tsx
@@ -1,53 +1,74 @@
+"use client"
 import Layout from '@/app/components/layout'
 import Rolledrop from '../../components/Rolledrop';
 import '../../styles/useradministration.scss'
 import '../../styles/globals.scss'
+import { SetStateAction, useEffect, useState } from 'react';
+import { User } from '@/features/users/types';
 
 
 
 const useradministration = () => {
+
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async () => {
+        const response = await fetch('/api/users')
+        if (response.status == 200) {
+            const data = await response.json()
+            setUsers(data.data)
+        }
+        else
+            console.error("Something went wrong retrieving users from API")
+    }
+
+    const changeRole = (event: React.ChangeEvent<HTMLSelectElement>, user: User) => {
+        console.log("inside changerole")
+        console.log(users)
+        console.log(user)
+        setUsers(users?.map(el => (
+            el.email === user.email ? { ...el, role: event.target.value as User["role"] } : el
+        )))
+        console.log(users)
+    }
+
+
     return (
         <Layout>
-        <div className="mainDiv">
-            <h1>Brukeradministrasjon</h1>
-            <div className='mainbox'>
-                <article className='itembox'>
-                    <section className='contentitembox'><p>BRUKER</p></section>
-                    <section className='contentitemboxrolle'><Rolledrop/></section>
-                </article>
+            <div className="mainDiv">
+                <h1>Brukeradministrasjon</h1>
+                <div className='mainbox'>
 
-                <article className='itembox'>
-                    <section className='contentitembox'><p>BRUKER</p></section>
-                    <section className='contentitemboxrolle'><Rolledrop/></section>
-                </article>
+                    {users.map((user: User) =>
+                        <article className='itembox' key={user.email}>
+                            <section className='contentitembox'><p>{user.name}</p></section>
+                            <section className='contentitemboxrolle'><Rolledrop user={user} changeRole={changeRole} /></section>
+                        </article>
+                    )}
 
-                <article className='itembox'>
-                    <section className='contentitembox'><p>BRUKER</p></section>
-                    <section className='contentitemboxrolle'><Rolledrop/></section>
-                </article>
 
-                <article className='centerbox'>
-                    <button className='generatebutton'>Oppdater</button>
-                </article>
+                </div>
+
+                <div className='mainbox'>
+                    <article className='itembox2'>
+                        <section className='contentitemboxrolle'><input type='text' placeholder='Brukernavn' className='inputbruker'></input></section>
+                        <section className='contentitemboxrolle'><input type='text' placeholder='Passord' className='inputbruker'></input></section>
+                    </article>
+
+                    <article className='centerbox'>
+                        <section className='contentitemboxrolle'><Rolledrop /></section>
+                    </article>
+
+                    <article className='centerbox'>
+                        <button className='generatebutton'>Opprett profil</button>
+                    </article>
+                </div>
 
             </div>
-
-            <div className='mainbox'>
-            <article className='itembox2'>
-                    <section className='contentitemboxrolle'><input type='text' placeholder='Brukernavn' className='inputbruker'></input></section>
-                    <section className='contentitemboxrolle'><input type='text' placeholder='Passord' className='inputbruker'></input></section>
-                </article>
-
-                <article className='centerbox'>
-                    <section className='contentitemboxrolle'><Rolledrop/></section>
-                </article>
-
-                <article className='centerbox'>
-                    <button className='generatebutton'>Opprett profil</button>
-                </article>
-            </div>
-
-        </div>
         </Layout>
     )
 }
--- src/app/components/Rolledrop.tsx
+++ src/app/components/Rolledrop.tsx
@@ -1,9 +1,15 @@
-const Rolledrop = () => {
-    return(
-        <select name="rolle" id="rolle"> 
-        <option value="administrator">Admin</option> 
-        <option value="kitchen">Kjøkken</option> 
-        <option value="healthcare">Helsefagarbeider</option>
+import { User } from "@/features/users/types"
+import { Dispatch, SetStateAction } from "react"
+
+const Rolledrop = (props: { user: User, changeRole: (event: React.ChangeEvent<HTMLSelectElement>, user: User) => void }) => {
+
+    const { user, changeRole } = props
+
+    return (
+        <select name="rolle" id="rolle" value={user?.role} onChange={(e) => changeRole(e, user)}>
+            <option value="administrator">Admin</option>
+            <option value="kitchen">Kjøkken</option>
+            <option value="healthcare">Helsefagarbeider</option>
         </select>
     )
 }
