266711f0c4d494dbbc2fea31fb16e50432fc861a
Håkon <hakons>
2024-03-13 14:39:45 +0100

    Refactored out category rolldown menu

---
+++ src/app/components/MealCategoryRolldownMenu.tsx
@@ -0,0 +1,17 @@
+import { Meal } from "@/features/meals/types"
+import { ChangeEvent } from "react"
+
+const MealCategoryRolldownMenu = (props: { meal: Meal, handleCategoryChange: (event: ChangeEvent<HTMLSelectElement>) => void }) => {
+    const { meal, handleCategoryChange } = props
+    return (
+        <select id="category" onChange={handleCategoryChange}>
+            {meal.category === 'undefined' ? <option>Velg kategori:</option> : ''}
+            <option value="red meat">Rødt kjøtt</option>
+            <option value="fish">Fisk</option>
+            <option value="chicken">Kylling</option>
+            <option value="pasta">Pasta</option>
+            <option value="vegetable">Grønnsak</option>
+        </select>
+    )
+}
+export default MealCategoryRolldownMenu
