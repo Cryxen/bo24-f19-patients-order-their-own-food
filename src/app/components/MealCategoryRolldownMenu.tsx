c4a0ade3bb0c3b7054b0242f57cf26a9bf5b9ef7
Håkon <hakons>
2024-03-13 17:38:56 +0100

    refactored for readability

--- src/app/components/MealCategoryRolldownMenu.tsx
+++ src/app/components/MealCategoryRolldownMenu.tsx
@@ -11,8 +11,10 @@ const MealCategoryRolldownMenu = (props: { meal?: Meal, handleCategoryChange: (e
 
     return (
         <select id="category" value={meal.category} onChange={handleCategoryChange}>
-            {filter ? <option value="Show all">Vis alle</option> : //If filter is false, show 'velg kategori', if filter is true, show 'vis alle'
-                meal.category === 'undefined' ? <option>Velg kategori:</option> : ''}
+            {
+                filter ? <option value="Show all">Vis alle</option> : // if filter is true, show 'vis alle', 
+                    meal.category === 'undefined' ? <option>Velg kategori:</option> : '' //If filter is false, show 'velg kategori'
+            }
             <option value="red meat">Rødt kjøtt</option>
             <option value="fish">Fisk</option>
             <option value="chicken">Kylling</option>
