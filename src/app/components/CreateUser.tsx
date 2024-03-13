b51e76a3db413056f6bb6d3fc3b0ba7d696a32c8
Håkon <hakons>
2024-03-13 09:20:42 +0100

    made useState to handle user object

--- src/app/components/CreateUser.tsx
+++ src/app/components/CreateUser.tsx
@@ -1,19 +1,48 @@
+"use client"
+import { ChangeEvent, useState } from "react"
+import Rolledrop from "./Rolledrop"
+import { User } from "@/features/users/types"
+
 const CreateUser = () => {
-return (
-    <div className='mainbox'>
-    <article className='itembox2'>
-        <section className='contentitemboxrolle'><input type='text' placeholder='Brukernavn' className='inputbruker'></input></section>
-        <section className='contentitemboxrolle'><input type='text' placeholder='Passord' className='inputbruker'></input></section>
-    </article>
+    const [user, setUser] = useState<User>(
+        {
+            name: '',
+            email: '',
+            password: '',
+            role: 'undefined'
+        }
+    )
 
-    <article className='centerbox'>
-        <section className='contentitemboxrolle'><Rolledrop /></section>
-    </article>
+    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setUser(prev => ({
+            ...prev, name: event.target.value
+        }))
+    }
+    const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setUser(prev => ({
+            ...prev, password: event.target.value
+        }))
+    }
 
-    <article className='centerbox'>
-        <button className='generatebutton'>Opprett profil</button>
-    </article>
-</div>
-)
+    return (
+        <div className='mainbox'>
+            <article className='itembox2'>
+                <section className='contentitemboxrolle'>
+                    <input type='text' placeholder='Brukernavn' className='inputbruker' value={user.name} onChange={handleNameChange} />
+                </section>
+                <section className='contentitemboxrolle'>
+                    <input type='text' placeholder='Passord' className='inputbruker' value={user.password} onChange={handlePasswordChange} />
+                </section>
+            </article>
+
+            <article className='centerbox'>
+                <section className='contentitemboxrolle'><Rolledrop /></section>
+            </article>
+
+            <article className='centerbox'>
+                <button className='generatebutton'>Opprett profil</button>
+            </article>
+        </div>
+    )
 }
 export default CreateUser
