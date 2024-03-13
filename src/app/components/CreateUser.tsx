921b468e48c1fc649ac6e1814aa85b75f4111e9c
Håkon <hakons>
2024-03-13 09:31:33 +0100

    made create user function

--- src/app/components/CreateUser.tsx
+++ src/app/components/CreateUser.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, MouseEvent, useState } from "react"
 import Rolledrop from "./Rolledrop"
 import { User } from "@/features/users/types"
 
@@ -18,23 +18,54 @@ const CreateUser = () => {
             ...prev, name: event.target.value
         }))
     }
+    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setUser(prev => ({
+            ...prev, email: event.target.value
+        }))
+    }
     const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
         setUser(prev => ({
             ...prev, password: event.target.value
         }))
     }
 
-    const changeRole = (event: ChangeEvent<HTMLSelectElement>, user:User) => {
+    const changeRole = (event: ChangeEvent<HTMLSelectElement>, user: User) => {
         setUser(prev => ({
             ...prev, role: event.target.value as User["role"]
         }))
     }
 
+    const submitUser = async (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        if (user.name !== '' || user.email !== '' || user.password !== '' || user.role !== 'undefined') //If check to see that fields are filled
+        {
+            const response = await fetch('/api/users', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            })
+            if (response.status === 200)
+            {
+                const data = await response.json()
+                console.log(data)
+            }
+            else
+                console.error("Something went wrong creating user API call")
+        }
+        console.log(user)
+    }
+
+
     return (
         <div className='mainbox'>
             <article className='itembox2'>
                 <section className='contentitemboxrolle'>
-                    <input type='text' placeholder='Brukernavn' className='inputbruker' value={user.name} onChange={handleNameChange} />
+                    <input type='text' placeholder='Navn' className='inputbruker' value={user.name} onChange={handleNameChange} />
+                </section>
+                <section className='contentitemboxrolle'>
+                    <input type='email' placeholder='E-post' className='inputbruker' value={user.email} onChange={handleEmailChange} />
                 </section>
                 <section className='contentitemboxrolle'>
                     <input type='password' placeholder='Passord' className='inputbruker' value={user.password} onChange={handlePasswordChange} />
@@ -42,11 +73,11 @@ const CreateUser = () => {
             </article>
 
             <article className='centerbox'>
-                <section className='contentitemboxrolle'><Rolledrop user={user} changeRole={changeRole}/></section>
+                <section className='contentitemboxrolle'><Rolledrop user={user} changeRole={changeRole} /></section>
             </article>
 
             <article className='centerbox'>
-                <button className='generatebutton'>Opprett profil</button>
+                <button className='generatebutton' onClick={submitUser}>Opprett profil</button>
             </article>
         </div>
     )
