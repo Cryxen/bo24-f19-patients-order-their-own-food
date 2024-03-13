20eb753b1a4023d5c11631089877d18f4bba19a8
Håkon <hakons>
2024-03-13 14:02:14 +0100

    made POST method

--- src/app/api/meals/route.ts
+++ src/app/api/meals/route.ts
@@ -4,3 +4,7 @@ import { NextRequest } from "next/server"
 export async function GET(req: NextRequest) {
     return await mealController.fetchAllMeals(req)
 }
+
+export async function POST (req: NextRequest) {
+    return await mealController.saveMeal(req)
+}
