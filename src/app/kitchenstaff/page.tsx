31b7accc3bf86b2096944d84d73dc07bb942dd51
HippedFever <Matsc99@gmail.com>
2024-03-14 14:22:53 +0100

    Navbox funskjon med oppdaterte dashboard

--- src/app/kitchenstaff/page.tsx
+++ src/app/kitchenstaff/page.tsx
@@ -1,6 +1,7 @@
 import '../styles/kitchenstaff.scss'
 import '../styles/globals.scss'
 import Layout from '../components/layout'
+import Navbox from '../components/Navbox'
 
 export default function Kitchenstaff() {
     return (
@@ -8,12 +9,9 @@ export default function Kitchenstaff() {
             <div className="mainDiv">
                 <h1>Kjøkken</h1>
                 <section className='dashboardwrap'>
-                <button>Måltid planlegging</button>
-                <button>Mat håndtering</button>
-                <button>Statistikk verktøy</button>
-                <button>Eventuelle knapper</button>
-                <button>Eventuelle knapper</button>
-                <button>Eventuelle knapper..</button>
+                <Navbox redirect='kitchenstaff/mealplanning' text='Måltid planlegging'/>
+                <Navbox redirect='kitchenstaff/foodmanagement' text='Mat håndtering'/>
+                <Navbox redirect='kitchenstaff/staffstatistics' text='Statistikk verktøy'/>
                 </section>
             </div>
         </Layout>
