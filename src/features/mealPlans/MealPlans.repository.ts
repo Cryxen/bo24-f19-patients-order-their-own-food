84d95ac221be1292139226dfb29d35a15192893d
Håkon <hakons>
2024-03-20 07:58:01 +0100

    made meals connected to mealPlan show

--- src/features/mealPlans/MealPlans.repository.ts
+++ src/features/mealPlans/MealPlans.repository.ts
@@ -4,7 +4,11 @@ const prisma = new PrismaClient()
 
 export const fetchAllMealPlans = async () => {
     try {
-        const mealPlansFromDb = await prisma.mealPlan.findMany()
+        const mealPlansFromDb = await prisma.mealPlan.findMany({
+            select: {
+                meals: true
+            }
+        })
         return {success: true, data: mealPlansFromDb}
     } catch (error) {
         return {success: false, error: "Failed to retrieve meal plans from db"}
