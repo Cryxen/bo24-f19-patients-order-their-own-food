4b60574d65778678da2c258ce664eb74fe855dc3
Håkon <hakons>
2024-03-22 09:38:50 +0100

    defined sideDish and mainDish

--- src/features/meals/types.ts
+++ src/features/meals/types.ts
@@ -1,9 +1,17 @@
 export type Meal = {
     mealName: string
     description: string
-    category: "red meat" | "fish" | "chicken" | "pasta" |"vegetable" | "undefined"
+    category: sideDish | mainDish | "undefined"
     dietaryInfo?: string[] | string;
     imageUrl?: string
+}
+
+export type sideDish = {
+    sideDish: "vegetable"
+}
+
+export type mainDish = {
+    mainDish: "red meat" | "fish" | "chicken" | "pasta" 
 }
 
 export const dietaryRestrictions = ['Red meat', 'Salt', 'Gluten']
