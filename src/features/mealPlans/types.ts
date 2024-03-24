0ce47e903f4b08d7b40b7c5030dd7faf25b6a2f8
Håkon <hakons>
2024-03-24 15:24:04 +0100

    made Meals fit better the return from database

--- src/features/mealPlans/types.ts
+++ src/features/mealPlans/types.ts
@@ -1,9 +1,10 @@
 import { Meal } from "../meals/types"
 
 export type MealPlan = {
- meal: Meal[]
- date: Date | string
- imageUrl?: string
- description: string   
- order?: undefined //TODO: Implement order type and place here.
+    //  meals: Meal[]
+    meals: Array<{[key: string]: Meal}>
+    date: Date | string
+    imageUrl?: string
+    description: string
+    order?: undefined //TODO: Implement order type and place here.
 }
