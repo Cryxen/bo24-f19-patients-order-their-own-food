e4729a262adee7dacdd1104fd2dd4f6dc393d1ba
Håkon <hakons>
2024-03-24 18:52:59 +0100

    refactored out commented code

--- src/app/kitchenstaff/mealplanning/page.tsx
+++ src/app/kitchenstaff/mealplanning/page.tsx
@@ -49,38 +49,7 @@ const Mealplanning = () => {
                     </div>
                     :
                     <NewMealPlan />
-                    // <div className="container">
-                    //     <article>
-                    //         <h3>
-                    //             Hovedrett:
-                    //         </h3>
-                    //         <form>
-                    //             {mealList?.map(meal =>
-                    //                 (MAIN_DISH as unknown as MainDish[]).includes(meal.category as MainDish) ?
-                    //                     <div key={meal.mealName}>
-                    //                         <label htmlFor={meal.mealName}>{meal.mealName}</label>
-                    //                         <input type="radio" id={meal.mealName} value={meal.mealName} name="mainDish" />
-                    //                     </div>
-                    //                     : ''
-                    //             )}
-                    //         </form>
-                    //     </article>
-                    //     <article>
-                    //         <h3>
-                    //             Siderett:
-                    //         </h3>
-                    //         <form>
-                    //             {mealList?.map(meal =>
-                    //                 (SIDE_DISH as unknown as SideDish[]).includes(meal.category as SideDish) ?
-                    //                     <div key={meal.mealName}>
-                    //                         <label htmlFor={meal.mealName}>{meal.mealName}</label>
-                    //                         <input type="radio" id={meal.mealName} value={meal.mealName} name="sideDish" />
-                    //                     </div>
-                    //                     : ''
-                    //             )}
-                    //         </form>
-                    //     </article>
-                    // </div>
+                   
                 }
                 <div className="break" />
                 <article className="nutrition">
