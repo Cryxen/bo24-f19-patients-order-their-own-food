3422f5a486abc0b0d03f11964f7a6e9c0ef536a9
Håkon <hakons>
2024-03-27 18:48:02 +0100

    init updating meals

--- src/app/components/MealPlanList.tsx
+++ src/app/components/MealPlanList.tsx
@@ -5,6 +5,7 @@ import { ChangeEvent, MouseEvent, useEffect, useState } from 'react'
 
 const MealPlanList = (props: { mealPlan: MealPlan }) => {
     const [updateMealPlanMode, setUpdateMealPlanMode] = useState<boolean>(false)
+    const [listOfMeals, setListOfMeals] = useState<Meal[]>([])
     const [mealPlanToUpdate, setMealPlanToUpdate] = useState<MealPlan>({
         description: '',
         meals: [],
@@ -19,6 +20,7 @@ const MealPlanList = (props: { mealPlan: MealPlan }) => {
     const updateMealButton = (event: MouseEvent<HTMLButtonElement>): void => {
         event.preventDefault()
         setUpdateMealPlanMode(!updateMealPlanMode)
+        console.log(mealPlanToUpdate.meals[0].meal)
     }
 
     const updateDescriptionField = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -27,6 +29,9 @@ const MealPlanList = (props: { mealPlan: MealPlan }) => {
         }))
     }
 
+    const handleMainDishChange = () => {
+
+    }
 
     return (
         <article className="meal-plan">
@@ -38,7 +43,13 @@ const MealPlanList = (props: { mealPlan: MealPlan }) => {
                 <section className='information'>
                     <label htmlFor='descriptionField'>Beskrivelse:</label>
                     <input type='text' value={mealPlanToUpdate?.description} onChange={updateDescriptionField} id='descriptionField'/>
-                    
+                    <p>Hovedrett: </p>
+                    {mealPlanToUpdate.meals?.map(meal =>
+                        (MAIN_DISH as unknown as MainDish[]).includes(meal.meal.category as MainDish) ?
+                            <div key={meal.meal.mealName as string}>
+                                <label htmlFor={meal.meal.mealName as string}>{meal.meal.mealName as string}</label>
+                                <input type="radio" id={meal.meal.mealName as string} value={meal.meal.mealName as string} name="mainDish" onClick={handleMainDishChange} />
+                            </div> : '' )}
                 </section>
                 :
                 <section className="information">
