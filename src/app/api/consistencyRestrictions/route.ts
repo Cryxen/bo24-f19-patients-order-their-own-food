2d7559e2543e45724f08a9ccc9064edfd5ea6870
Håkon <hakons>
2024-03-31 11:37:45 +0200

    made GET APi for consistency restrictions

---
+++ src/app/api/consistencyRestrictions/route.ts
@@ -0,0 +1,6 @@
+import * as consistencyRestrictionsController from '@/features/consistencyRestrictions/ConsistencyRestrictions.controller'
+import { NextRequest } from 'next/server'
+
+export const GET = async (req: NextRequest) => {
+    return consistencyRestrictionsController.fetchAllConsistencyRestrictions()
+}
