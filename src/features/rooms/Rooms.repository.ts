7567b2eff501a473ef1be9852a747d5dd00e86c7
Håkon <hakons>
2024-03-31 11:00:50 +0200

    made initial API and backend

--- src/features/rooms/Rooms.repository.ts
+++ src/features/rooms/Rooms.repository.ts
@@ -3,9 +3,27 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 export const fetchAllRooms = async () => {
     try {
-        const roomsFromDb = await prisma.room.findMany()
-        return {success: true, data: roomsFromDb}
+        const roomsFromDb = await prisma.room.findMany({
+            include: {
+                dietaryRestrictions: {
+                    select: { dietaryRestrictionId: true }
+                },
+                foodConsistencyRestrictions: {
+                    select: { foodConsistencyRestrictionId: true }
+                },
+                RoomToAllergyRestrictions: {
+                    select: { allergyRestricionId: true }
+                },
+                RoomToIntolleranceRestrictions: {
+                    select: { intolleranceRestrictionId: true }
+                },
+                RoomToDietaryneeds: {
+                    select: { dietaryNeedId: true }
+                }
+            }
+        })
+        return { success: true, data: roomsFromDb }
     } catch (error) {
-        return {success: false, error: "Failed to retrieve users from db " + error}
+        return { success: false, error: "Failed to retrieve users from db " + error }
     }
 }
--- src/features/rooms/Rooms.service.ts
+++ src/features/rooms/Rooms.service.ts
@@ -1,9 +1,9 @@
-import * as roomsRepo from './rooms.repository'
+import * as roomsRepo from './Rooms.repository'
 
 export const fetchAllRooms = async () => { 
     try {
         const roomsFromDb = await roomsRepo.fetchAllRooms();
-        return {success: true, data: roomsFromDb.data}
+        return {success: true, data: roomsFromDb.data, error: roomsFromDb.error}
     } catch (error) {
         return {success: false, data: "Failed to fetch rooms from db in service " + error}
     }
