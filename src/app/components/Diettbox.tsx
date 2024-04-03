2eaee277029bc4a055af266df7997d2a0bca7fc5
Håkon <hakons>
2024-04-03 10:55:04 +0200

    made a onChange method

--- src/app/components/Diettbox.tsx
+++ src/app/components/Diettbox.tsx
@@ -1,12 +1,14 @@
+import { ChangeEvent } from "react"
+
 // const Diettbox = ({ Diett }: { Diett: string }, { Checked }: { Checked: boolean }) => {
-const Diettbox = (props: { Diett: string, Checked: boolean }) => {
-    const { Diett, Checked } = props
+const Diettbox = (props: { Diett: string, Checked: boolean, onChangeMethod?: (event: ChangeEvent<HTMLInputElement>) => void }) => {
+    const { Diett, Checked, onChangeMethod } = props
     // console.log(Diett + " : " + Checked) //FOR DEBUG
     return (
         <div className="diet-container">
             <p>{Diett}</p>
             <label className="switch">
-                <input type="checkbox" name="toggleSwitch" checked={Checked ? true : false} />
+                <input type="checkbox" name="toggleSwitch" checked={Checked ? true : false} onChange={onChangeMethod} value={Diett}/>
                 <span className="slider round"></span>
             </label>
         </div>
