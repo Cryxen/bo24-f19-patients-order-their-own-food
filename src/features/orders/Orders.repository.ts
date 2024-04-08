9a9c4b182f01ae8adb7263b7f66c11f19d97dcfd
Håkon <hakons>
2024-04-08 09:05:12 +0200

    made mealPlan show in API

--- src/features/orders/Orders.repository.ts
+++ src/features/orders/Orders.repository.ts
@@ -5,7 +5,11 @@ const prisma = new PrismaClient
 export const fetchAllOrders = async () => {
 
     try {
-        const fetchOrdersFromDb = await prisma.order.findMany({})
+        const fetchOrdersFromDb = await prisma.order.findMany({
+            include: {
+                mealPlan: true
+            }
+        })
         return ({ success: true, data: fetchOrdersFromDb })
     } catch (error) {
         return ({ success: false, error: "Failed to fetch orders from db in repository " + error })
