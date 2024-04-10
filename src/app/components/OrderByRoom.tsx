66bc9e4e098c4d65d3175e2f1abcc44e1eac28e6
Håkon <hakons>
2024-04-10 10:23:37 +0200

    made commentary field

--- src/app/components/OrderByRoom.tsx
+++ src/app/components/OrderByRoom.tsx
@@ -2,12 +2,12 @@ import { MealPlan } from "@/features/mealPlans/types"
 import { MAIN_DISH, MainDish } from "@/features/meals/types"
 import { OrderClass } from "@/features/orders/classes"
 import { Order } from "@/features/orders/types"
-import { MouseEvent, useState } from "react"
+import { ChangeEvent, MouseEvent, useState } from "react"
 
 const OrderByRoom = (props: { ordersByRoom: Order[] }) => {
     const [showChangeOrder, setShowChangeOrder] = useState<boolean>(false)
     const [mealPlansByDate, setMealPlansByDate] = useState<MealPlan[]>([])
-
+    const [newMealPlanToUpdate, setNewMealPlanToUpdate] = useState<MealPlan>()
     const { ordersByRoom } = props
 
     const fetchMealPlansByDate = async () => {
@@ -15,7 +15,6 @@ const OrderByRoom = (props: { ordersByRoom: Order[] }) => {
         const response = await fetch('/api/mealPlans?date=' + date.toDateString())
         if (response.status === 200) {
             const data = await response.json()
-            console.log(data)
             setMealPlansByDate(data.data)
         }
     }
@@ -23,6 +22,11 @@ const OrderByRoom = (props: { ordersByRoom: Order[] }) => {
     const handleOrderChangeButton = async (event: MouseEvent<HTMLButtonElement>) => {
         await fetchMealPlansByDate()
         setShowChangeOrder(!showChangeOrder)
+    }
+
+    const handleMealPlanChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const chosenMealPlan = mealPlansByDate.filter(el => el.id === parseInt(event.target.value))
+        setNewMealPlanToUpdate(chosenMealPlan[0])
     }
 
     return (
@@ -35,20 +39,23 @@ const OrderByRoom = (props: { ordersByRoom: Order[] }) => {
                         showChangeOrder ?
                             <form key={order.id}>
                                 <label htmlFor="todaysMealPlans">Velg rett:</label>
-                                <select name="todaysMealPlans" id="todaysMealPlans">
+                                <select name="todaysMealPlans" id="todaysMealPlans" onChange={handleMealPlanChange}>
+                                    <option value={0}>
+                                        Velg nytt måltid:
+                                    </option>
                                     {mealPlansByDate.map(el => {
                                         let mainDish: string = 'undefined'
                                         let sideDish: string = 'undefined'
                                         el.meals.map(meal => {
                                             (MAIN_DISH as unknown as MainDish[]).includes(meal.meal.category as unknown as MainDish) ?
                                                 mainDish = meal.meal.mealName as string : sideDish = meal.meal.mealName as string
-                                            console.log(meal.meal)
                                         })
                                         return (<option key={el.id} value={el.id}>
                                             Hovedrett: {mainDish} - Siderett: {sideDish}
                                         </option>)
                                     })}
                                 </select>
+                                <p>Kommentar: {newMealPlanToUpdate?.description}</p>
                             </form>
                             :
                             <p className="dish" key={order.id}>{orderClass.mealPlan.description} - {orderClass.mealSize}</p>
