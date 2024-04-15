c4de522e566e0610059597ebe79b2be8afe29c18
Håkon <hakons>
2024-04-15 17:46:16 +0200

    made route to dashboard function

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,32 @@
-import { checkUserRole } from "@/libs/router/checkUserRole"
-import { useEffect } from "react"
+import { findRoleFromCookies } from "@/libs/cookies/userRolesClient"
+import { useRouter } from "next/navigation"
 
 const Header = () => {
+    const router = useRouter()
 
+    const routeToDashboard = () => {
+        const role = findRoleFromCookies()
+        switch (role) {
+            case 'administrator':
+                router.push('/administrator')
+                break;
+            case 'development':
+                router.push('/')
+                break;
+            case "healthcare":
+                router.push('/healthcareworker')
+                break;
+            case "kitchen":
+                router.push('/kitchenstaff')
+                break;
+            default:
+                break;
+        }
+    }
 
-
-    return(
+    return (
         <header>
-            <button className="header-content">Dashboard</button>
+            <button className="header-content" onClick={routeToDashboard}>Dashboard</button>
             <button className="header-content">Bruker</button>
         </header>
     )
