d6cc2d9ae2790211ec084a14ee94dbbeb5eef591
Håkon <hakons>
2024-04-15 12:02:42 +0200

    made a check of link of origin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { checkUserRole } from "./libs/router/checkUserRole";
 import { NextApiResponse } from "next";
+import { checkPathOfOrigin } from "./libs/router/checkPathOfOrigin";
 
 export const middleware = (req: NextRequest, res: NextResponse) => {
-    if (!checkUserRole(req)) {
+    if (!checkUserRole(req) && !checkPathOfOrigin(req, "/")) {
         return NextResponse.redirect(new URL('/', req.url))
-
     }
 }
 
