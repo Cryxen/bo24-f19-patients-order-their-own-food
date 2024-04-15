874aef0693094c931f146dc720849e57f053092b
Håkon <hakons>
2024-04-15 11:47:16 +0200

    removed debugg and old code

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,10 @@ import { checkUserRole } from "./libs/router/checkUserRole";
 import { NextApiResponse } from "next";
 
 export const middleware = (req: NextRequest, res: NextResponse) => {
-    console.log("Inside middleware")
     if (!checkUserRole(req)) {
         return NextResponse.redirect(new URL('/', req.url))
 
     }
-
-
-    // return checkUserRole(req, res)
 }
 
 export const config = {
