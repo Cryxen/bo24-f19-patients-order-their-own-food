8e249e2071e1bb1c8469689b55dca029d5d5e48c
Håkon <hakons>
2024-04-17 10:17:31 +0200

    made typeguard

--- src/features/users/types.ts
+++ src/features/users/types.ts
@@ -8,3 +8,18 @@ export type User = {
 export const userRoleTypeGuard = (value: any): value is User["role"] => {
     return value === 'undefined' || value === 'healthcare' || value === 'kitchen' || value === 'administrator' || value === 'development'
 }
+
+/**
+ * A typeguard to check if object is user.
+ * @param obj object to typeguard
+ * @returns true/false if object is user
+ */
+export const isUser = (obj: any): obj is User => {
+    return (
+        typeof obj === 'object' &&
+        typeof obj.email === 'string' &&
+        typeof obj.name === 'string' &&
+        (obj.role === 'healthcare' || obj.role === 'kitchen' || obj.role === 'administrator' || obj.role === 'undefined' || obj.role === 'development') &&
+        typeof obj.password === 'string'
+    )
+}
