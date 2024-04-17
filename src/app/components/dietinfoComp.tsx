93fe9e4ffdc3564fdf3ee4776fabfb96b8d78709
HippedFever <Matsc99@gmail.com>
2024-04-17 08:19:18 +0200

    Diettinfo på kitchen, trenger css forbedringer

--- src/app/components/dietinfoComp.tsx
+++ src/app/components/dietinfoComp.tsx
@@ -1,18 +1,114 @@
-const dietinfoComp = ({Rom, Diett}: {Rom: string; Diett: string}) => {
-    return(
+"use client"
 
-        <article className='gridLayout'>
+import React, { useEffect, useState } from 'react';
+import '../styles/minidiet.scss';
 
-            <div className='div1'>
-                <h1>{Rom}</h1>
-            </div>
-
-            <div className='div2'>
-                <p>{Diett}</p>
-            </div>
-
-        </article>
-
-    )
+interface Room {
+  roomNumber: number;
+  allergyRestrictions: string[];
+  consistencyRestrictions: string[];
+  dietaryNeeds: string[];
+  dietaryRestrictions: string[];
+  intoleranceRestrictions: string[];
 }
-export default dietinfoComp
+
+const DietInfoContainer = () => {
+    const [rooms, setRooms] = useState<Room[]>([]);
+  
+    useEffect(() => {
+      const fetchRooms = async () => {
+        try {
+          const response = await fetch('/api/rooms');
+    
+          if (!response.ok) {
+            throw new Error('Programmet feilet ved henting av rom');
+          }
+    
+          const roomsData = await response.json();
+          console.log('Hentede rom:', roomsData); 
+          setRooms(roomsData.data); 
+        } catch (error) {
+          console.error('Error ved henting a rom:', error);
+        }
+      };
+    
+      fetchRooms();
+    }, []);
+  
+  
+  
+
+  const DietInfoComp = ({ dietType, dietName }: { dietType: string; dietName: string }) => {
+    return (
+
+      <article className='Minibox'>
+        <div className="Topbox">
+          <p className='textDiet'>{dietType}</p>
+        </div>
+        
+        <div className="Bottombox">
+          <p className='textDiet'>{dietName}</p>
+        </div>
+      </article>
+
+    );
+  };
+
+  if (!Array.isArray(rooms) || rooms.length === 0) {
+    return <div>Listen er tom</div>;
+  }
+
+  return (
+    <div className='displayX'>
+      {rooms.map((room, index) => {
+
+        const hasRestrictions =
+          (room.allergyRestrictions && room.allergyRestrictions.length > 0) ||
+          (room.consistencyRestrictions && room.consistencyRestrictions.length > 0) ||
+          (room.dietaryNeeds && room.dietaryNeeds.length > 0) ||
+          (room.dietaryRestrictions && room.dietaryRestrictions.length > 0) ||
+          (room.intoleranceRestrictions && room.intoleranceRestrictions.length > 0);
+
+  
+        if (!hasRestrictions) {
+          return null;
+        }
+  
+        return (
+          <div key={index} className="gridLayout">
+            <div className='div1'>
+              <h1>Rom {room.roomNumber}</h1>
+            </div>
+            <div className='div2'>
+
+              {room.dietaryRestrictions.map((restriction, index) => (
+                <DietInfoComp key={index} dietType="Diettrestriksjoner" dietName={restriction.dietaryRestrictionId} />
+              ))}
+
+              {room.consistencyRestrictions && room.consistencyRestrictions.map((restriction, index) => (
+                <DietInfoComp key={index} dietType="Konsistensrestriksjoner" dietName={restriction} />
+              ))}
+
+              {room.allergyRestrictions.map((restriction, index) => (
+                restriction && <DietInfoComp key={index} dietType="Allergier" dietName={restriction.allergyRestricionId} />
+              ))}
+
+              {room.intoleranceRestrictions.map((restriction, index) => (
+                <DietInfoComp key={index} dietType="Intoleranser" dietName={restriction.intolleranceRestrictionId} />
+              ))}
+
+              {room.dietaryNeeds.map((restriction, index) => (
+                <DietInfoComp key={index} dietType="Andre kostbehov" dietName={restriction.dietaryNeedId} />
+              ))}
+
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+  
+  
+};
+
+export default DietInfoContainer;
--- src/app/kitchenstaff/dietaryinfo/page.tsx
+++ src/app/kitchenstaff/dietaryinfo/page.tsx
@@ -1,24 +1,15 @@
 import '../../styles/globals.scss'
-import '../../styles/dietaryinfo.scss'
 import Layout from '../../components/layout'
 import DietinfoComp from '@/app/components/dietinfoComp';
 
 export default function dietaryinfo() {
+
     return (
         <Layout>
             <div className="mainDiv">
                 <h1>Diett informasjon</h1>
-                <section className='dashboardwrap'>
-                
-                <DietinfoComp Rom='ROM 1223' Diett="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-                <DietinfoComp Rom='ROM 1224' Diett="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-                <DietinfoComp Rom='ROM 1225' Diett="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-                <DietinfoComp Rom='ROM 1226' Diett="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-                <DietinfoComp Rom='ROM 1227' Diett="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-                <DietinfoComp Rom='ROM 1228' Diett="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-                <DietinfoComp Rom='ROM 1229' Diett="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
 
-                </section>
+                        <DietinfoComp />
             </div>
         </Layout>
     )
