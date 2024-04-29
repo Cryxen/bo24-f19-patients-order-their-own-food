508a9fe1675a3a971dd4b6b422b484ceb6710693
Håkon <hakons>
2024-04-29 19:46:20 +0200

    fixed error

---
+++ src/libs/utils/globals.ts
@@ -0,0 +1,4 @@
+//Help from chatGPT
+import { PrismaClient } from '@prisma/client';
+
+export const globalPrisma: { prisma?: PrismaClient } = {};
--- src/libs/utils/prisma.ts
+++ src/libs/utils/prisma.ts
@@ -1,18 +1,20 @@
 //From: https://github.com/prisma/blogr-nextjs-prisma/blob/final/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
+import { globalPrisma } from "./globals";
 
 // Docs about instantiating `PrismaClient` with Next.js:
 // https://pris.ly/d/help/next-js-best-practices
+
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  if (!globalPrisma.prisma) {
+    globalPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalPrisma.prisma;
 }
 
 export default prisma;
