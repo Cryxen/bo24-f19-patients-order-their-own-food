37e5c40be8f412d23712f68ac252a53862d563b9
Håkon <hakons>
2024-05-01 06:28:04 +0200

    made feedback at login, to make it easier to see if db is unaccesable, or if it's wrong credentials

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -11,6 +11,8 @@ const Login = () => {
     //TODO: ERROR HANDLING.
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [showDbError, setShowDbError] = useState<boolean>(false)
+    const [showWrongCredentials, setShowWrongCredentials] = useState<boolean>(false)
 
     //https://nextjs.org/docs/pages/building-your-application/routing/redirecting
     const router = useRouter()
@@ -29,9 +31,13 @@ const Login = () => {
         event.preventDefault()
         let user: User
         const response = await fetch(`/api/users?email=${email}&password=${password}`)
+        console.log(response)
         if (response.status == 200) {
             const data = await response.json()
+            console.log(data)
             if (data.success === true) {
+                setShowDbError(false)
+                setShowWrongCredentials(false)
                 user = data.data
                 userRoleSaveToCookie(user.role)
                 //Route according to role
@@ -50,26 +56,41 @@ const Login = () => {
                         break;
                     default:
                         break;
-                }  
+                }
+            }
+            else if (data.success === false) {
+                setShowDbError(false)
+                setShowWrongCredentials(true)
+                console.error("feil passord")
+            }
+            else {
+                console.error("Mangler data.success")
+                setShowDbError(true)
+                setShowWrongCredentials(false)
             }
         }
     }
 
     return (
-            <div className='mainDiv'>
+        <div className='mainDiv'>
 
-                
-                <img src={'/media/logo-stor.webp'} alt='Stor Sunnaas logo' className='logoLogin'></img>
-                
-                <h2 className='titleLogin'>Matbestillingsplattform</h2>
 
-                <form className="login-container">
-                    <input type="email" value={email} onChange={handleEmailChange} placeholder='E-post' />
-                    <input type="password" value={password} onChange={handlePasswordChange} placeholder='Passord' />
-                    <button type="submit" className="submit-button" onClick={handleSubmitButton}>Login</button>
-                </form>
-            </div>
-            
+            <img src={'/media/logo-stor.webp'} alt='Stor Sunnaas logo' className='logoLogin'></img>
+
+            <h2 className='titleLogin'>Matbestillingsplattform</h2>
+            {showDbError ?
+                <p>Noe gikk galt med å koble til databasen. Av og til tar det litt tid når noe i infrastrukturen
+                    har fått en omstart, prøv igjen senere.</p> : ""}
+
+            {showWrongCredentials ?
+                <p>Kunne ikke finne brukernavn eller passord. Prøv på nytt</p> : ""}
+            <form className="login-container">
+                <input type="email" value={email} onChange={handleEmailChange} placeholder='E-post' />
+                <input type="password" value={password} onChange={handlePasswordChange} placeholder='Passord' />
+                <button type="submit" className="submit-button" onClick={handleSubmitButton}>Login</button>
+            </form>
+        </div>
+
     )
 }
 
